7a70ec94dd61f02f17e48ebe23163a0b3f39b2fe
seanlucius <seanlucius22@icloud.com>
2019-04-22 16:09:50 -0400

    react-simple-state-lab-done

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Cell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: this.props.value
+    }
+  }
+
+  changeColor = () => {
+    this.setState({
+      color: "#333"
+    })
+  }
+  render() {
+    return (
+      <div className="cell" style = {{backgroundColor: this.state.color}} onClick={this.changeColor}>
+
+      </div>
+    )
+  }
+}
+export default Cell
